72ad4b985feb0f5c1feab6a72be1e09457009cca
C7leo <149843920+C7leo@users.noreply.github.com>
2024-09-19 12:09:08 -0500

    feat(front-end): added initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front-end';
+
+  options = [
+    { path: '/home', title: 'Home'},
+    { path: '/plans', title: 'Plans'},
+    { path: '/order', title: 'Order'},
+    { path: '/profile', title: 'Profile'},
+
+  ]
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./public/pages/home/home.component";
+import {PlansComponent} from "./public/pages/plans/plans.component";
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {OrderComponent} from "./public/pages/order/order.component";
+import {ProfileComponent} from "./public/pages/profile/profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'plans', component: PlansComponent },
+  { path: 'order', component: OrderComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
+];
