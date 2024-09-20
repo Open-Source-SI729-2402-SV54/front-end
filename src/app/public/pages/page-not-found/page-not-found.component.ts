c407084f6a01666208b4a32f0694d26b658df627
C7leo <149843920+C7leo@users.noreply.github.com>
2024-09-19 22:21:02 -0500

    feat(front-end): add db.json

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
+  protected invalidUrl: string = '';
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private router: Router = inject(Router);
 
+  constructor() {
+    this.invalidUrl = '';
+  }
+  ngOnInit(): void {
+    this.invalidUrl = this.route.snapshot.url[0].path;
+  }
+
+  protected onNavigateHome() {
+    this.router.navigate(['home']).then();
+  }
 }
