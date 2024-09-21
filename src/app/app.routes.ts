0f7a10d2170c679b0f40e576eb895b9034e1a3a1
LuisVJ97 <u201717523@upc.edu.pe>
2024-09-21 16:29:17 -0500

    feat(order): update order component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,16 @@ import {PlansComponent} from "./nutrisend/pages/plans/plans.component";
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {OrderComponent} from "./nutrisend/pages/order/order.component";
 import {ProfileComponent} from "./nutrisend/pages/profile/profile.component";
+import {PayComponent} from "./nutrisend/pages/pay/pay.component";
+import {ScheduleComponent} from "./nutrisend/pages/schedule/schedule.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'plans', component: PlansComponent },
   { path: 'order', component: OrderComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'pay', component: PayComponent },
+  { path: 'schedule', component: ScheduleComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent}
 ];
--- src/app/nutrisend/pages/order/order.component.ts
+++ src/app/nutrisend/pages/order/order.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
+import {MatOption} from "@angular/material/core";
+import {MatFormField, MatLabel, MatSelect} from "@angular/material/select";
+import {MatButton} from "@angular/material/button";
+import {MatCardContent, MatCardHeader, MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    MatOption,
+    MatSelect,
+    MatLabel,
+    MatButton,
+    MatCardHeader,
+    MatCardContent,
+    MatFormField,
+    MatIconModule,
+    MatCardModule
+  ],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
 export class OrderComponent {
+  desayunoQuantity: number = 0;
+  almuerzoQuantity: number = 0;
+  cenaQuantity: number = 0;
+  totalAmount: number = 0.00;
+  selectedTime: string = '15:00 - 16:00';
+  availableTimes: string[] = [
+    '08:00 - 09:00', '09:00 - 10:00','10:00 - 11:00','11:00 - 12:00','12:00 - 13:00',
+    '13:00 - 14:00','14:00 - 15:00','15:00 - 16:00', '16:00 - 17:00','17:00 - 18:00',
+    '18:00 - 19:00','19:00 - 20:00',];
+  constructor(private router: Router) { }
 
+  increaseQuantity(type: string) {
+    if (type === 'desayuno') this.desayunoQuantity++;
+    if (type === 'almuerzo') this.almuerzoQuantity++;
+    if (type === 'cena') this.cenaQuantity++;
+    this.calculateTotal();
+  }
+
+  decreaseQuantity(type: string) {
+    if (type === 'desayuno' && this.desayunoQuantity > 0) this.desayunoQuantity--;
+    if (type === 'almuerzo' && this.almuerzoQuantity > 0) this.almuerzoQuantity--;
+    if (type === 'cena' && this.cenaQuantity > 0) this.cenaQuantity--;
+    this.calculateTotal();
+  }
+
+  calculateTotal() {
+    this.totalAmount = (this.desayunoQuantity * 10) +
+      (this.almuerzoQuantity * 25) +
+      (this.cenaQuantity * 26);
+  }
+
+  payNow() {
+    this.router.navigate(['/pay']);
+  }
+
+  goToSchedule() {
+    this.router.navigate(['/schedule']);
+  }
 }
