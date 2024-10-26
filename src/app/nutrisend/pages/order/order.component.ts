74efa0763e42afa9eb40543218d428a24d62e5a8
LuisVJ97 <u201717523@upc.edu.pe>
2024-10-25 21:18:25 -0500

    feat(order): update order services

--- src/app/nutrisend/pages/order/order.component.ts
+++ src/app/nutrisend/pages/order/order.component.ts
@@ -1,26 +1,197 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faAppleAlt, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import {Component, OnInit} from '@angular/core';
+import {OrderItem} from "../../model/order.entity";
+import {OrderService} from "../../services/order.service";
+import {NgForOf, NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [
+    NgForOf,
+    NgIf,
+    FormsModule,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatToolbar,
+    MatLabel,
+    MatInput,
+    MatButton,
+    MatIcon,
+    RouterLink
+  ],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  breakfasts: any[] = [];
+  lunches: any[] = [];
+  dinners: any[] = [];
+  selectedBreakfast: any;
+  selectedLunch: any;
+  selectedDinner: any;
+  breakfastCount: number = 1;
+  lunchCount: number = 1;
+  dinnerCount: number = 1;
+  selectedTime: string = '15:00 - 16:00';
+  availableTimes: string[] = [
+    '08:00 - 09:00', '09:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00', '12:00 - 13:00',
+    '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00',
+    '18:00 - 19:00', '19:00 - 20:00',
+  ];
 
-  constructor(library: FaIconLibrary , private router: Router) {
-    library.addIcons(faAppleAlt, faUtensils);
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    this.loadBreakfasts();
+    this.loadLunches();
+    this.loadDinners();
   }
 
-  navigateToFood(): void {
-    this.router.navigate(['/food']).then(r => console.log('navigated to food'));
+  loadBreakfasts(): void {
+    this.orderService.getBreakfasts().subscribe(
+      (data) => {
+        this.breakfasts = data;
+        console.log(this.breakfasts);
+      },
+      (error) => {
+        console.error('Error fetching breakfasts', error);
+      }
+    );
   }
-  navigateToFitFood(): void {
-    this.router.navigate(['/food-fit']).then(r => console.log('navigated to food-fit'));
+
+  loadLunches(): void {
+    this.orderService.getLunches().subscribe(
+      (data) => {
+        this.lunches = data;
+        console.log(this.lunches);
+      },
+      (error) => {
+        console.error('Error fetching lunches', error);
+      }
+    );
+  }
+
+  loadDinners(): void {
+    this.orderService.getDinners().subscribe(
+      (data) => {
+        this.dinners = data;
+        console.log(this.dinners);
+      },
+      (error) => {
+        console.error('Error fetching dinners', error);
+      }
+    );
+  }
+
+  onBreakfastSelect(id: string): void {
+    if (id) {
+      this.orderService.getBreakfastById(+id).subscribe(
+        (data) => {
+          this.selectedBreakfast = data;
+        },
+        (error) => {
+          console.error('Error fetching breakfast by ID', error);
+        }
+      );
+    } else {
+      this.selectedBreakfast = null;
+    }
+  }
+
+  onLunchSelect(id: string): void {
+    if (id) {
+      this.orderService.getLunchById(+id).subscribe(
+        (data) => {
+          this.selectedLunch = data;
+        },
+        (error) => {
+          console.error('Error fetching lunch by ID', error);
+        }
+      );
+    } else {
+      this.selectedLunch = null;
+    }
+  }
+
+  onDinnerSelect(id: string): void {
+    if (id) {
+      this.orderService.getDinnerById(+id).subscribe(
+        (data) => {
+          this.selectedDinner = data;
+        },
+        (error) => {
+          console.error('Error fetching dinner by ID', error);
+        }
+      );
+    } else {
+      this.selectedDinner = null;
+    }
+  }
+
+  incrementBreakfastCount(): void {
+    this.breakfastCount++;
+  }
+
+  decrementBreakfastCount(): void {
+    if (this.breakfastCount > 1) {
+      this.breakfastCount--;
+    }
+  }
+
+  incrementLunchCount(): void {
+    this.lunchCount++;
+  }
+
+  decrementLunchCount(): void {
+    if (this.lunchCount > 1) {
+      this.lunchCount--;
+    }
+  }
+
+  incrementDinnerCount(): void {
+    this.dinnerCount++;
+  }
+
+  decrementDinnerCount(): void {
+    if (this.dinnerCount > 1) {
+      this.dinnerCount--;
+    }
+  }
+
+  getTotal(): number {
+    const breakfastTotal = this.selectedBreakfast ? this.selectedBreakfast.price * this.breakfastCount : 0;
+    const lunchTotal = this.selectedLunch ? this.selectedLunch.price * this.lunchCount : 0;
+    const dinnerTotal = this.selectedDinner ? this.selectedDinner.price * this.dinnerCount : 0;
+
+    return breakfastTotal + lunchTotal + dinnerTotal;
+  }
+  pay(): void {
+    const totalAmount = this.getTotal();
+
+    if (totalAmount > 0) {
+      console.log(`Payment processed for total amount: $${totalAmount}`);
+
+      this.selectedBreakfast = null;
+      this.selectedLunch = null;
+      this.selectedDinner = null;
+
+      this.breakfastCount = 1;
+      this.lunchCount = 1;
+      this.dinnerCount = 1;
+      window.location.reload();
+
+      alert(`Payment of $${totalAmount} has been processed!`);
+    } else {
+      alert('Please select at least one item to pay.');
+    }
   }
 }
