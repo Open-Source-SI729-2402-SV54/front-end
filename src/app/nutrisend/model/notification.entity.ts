ac94018ce005b3ca70020f94260efbd512471d15
C7leo <149843920+C7leo@users.noreply.github.com>
2024-11-15 03:28:05 -0500

    feat(notifications): update

--- src/app/nutrisend/model/notification.entity.ts
+++ src/app/nutrisend/model/notification.entity.ts
@@ -1,10 +1,12 @@
-import {ScheduleMeals} from "./schedule-meals.entity";
-
-export class Notifications {
+export class Notification {
   id: number;
   userId: number;
   email: string;
-  schedule: ScheduleMeals[];
+  schedule: Array<{
+    typeMealId: number;
+    hour: string;
+    status: string;
+  }>;
 
   constructor() {
     this.id = 0;
